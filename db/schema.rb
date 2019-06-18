3b5e3538bbda49bf9234588bd4da0f8b8dfcb23d
Anthony Rurup <tcrurup@gmail.com>
2019-06-18 19:24:18 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190618190728) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.float "balance"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
