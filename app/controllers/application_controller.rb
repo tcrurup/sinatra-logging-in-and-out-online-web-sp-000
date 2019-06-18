382b4aa25b2b4aaf89d7e8bd45d9b03fc2ef6576
Anthony Rurup <tcrurup@gmail.com>
2019-06-18 20:19:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    user = User.where(username: params[:username])
+    user = User.find(username: params[:username])
     puts user
     session[:user_id] = user.id
     redirect to '/account'
